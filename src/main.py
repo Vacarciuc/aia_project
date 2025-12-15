d11eb361ea6901ec8f6fb36cfdfa3f618d7cac55
Vacarciuc <ivacarciuc0@gmail.com>
2025-12-15 19:56:33 +0200

    fixed: success read data from .csv

--- src/main.py
+++ src/main.py
@@ -1,1 +1,20 @@
-print("Init app!!!")
+import WeatherDataLoader as WDL
+
+
+def main():
+    path = "C:/Users/enigma/Desktop/AIA/data sets"
+
+    loader = WDL.WeatherDataLoader(
+        root_path=path,
+        file_names=[
+            '72529014768',
+        ]
+    )
+
+    data = loader.load_files_from_all_folders()
+
+    print(data)
+
+
+if __name__ == '__main__':
+    main()
